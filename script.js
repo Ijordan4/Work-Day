c47124f73f91dfa278748b725681327bb5751766
Ijordan4 <124106922+Ijordan4@users.noreply.github.com>
2023-07-06 14:46:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $(function () {
-
+  // Click event listener for save button
   $('.saveBtn').on('click', function() {
     var timeBlockId = $(this).parent().attr('id');
     var userInput = $(this).siblings('.description').val();
     localStorage.setItem(timeBlockId, userInput);
   });
   
-  
+  // Apply past, present, or future class to each time block
   var currentHour = dayjs().hour();
   $('.time-block').each(function() {
     var blockHour = parseInt($(this).attr('id').split('-')[1]);
@@ -19,14 +19,23 @@ $(function () {
     }
   });
   
-  
+ 
   $('.time-block').each(function() {
     var timeBlockId = $(this).attr('id');
     var userInput = localStorage.getItem(timeBlockId);
     $(this).find('.description').val(userInput);
   });
   
-  
+  // Display current date in the header of the page
   var currentDate = dayjs().format('dddd, MMMM D, YYYY');
   $('#currentDay').text(currentDate);
+});
+
+
+$(window).on('load', function() {
+  $('.time-block').each(function() {
+    var timeBlockId = $(this).attr('id');
+    var userInput = localStorage.getItem(timeBlockId);
+    $(this).find('.description').val(userInput);
+  });
 });
